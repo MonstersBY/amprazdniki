3989b2efcf791e28c938345ea929ebd8261ea9f9
Kie6er <kirill.zawgorodny@yandex.ru>
2023-10-13 18:33:31 +0300

    feat: modals

---
+++ src/js/components/modal.js
@@ -0,0 +1,22 @@
+$(document).ready(function () {
+
+	$(document).on('keyup', (evt) => {
+		evt.keyCode === 27 ? closeModal() : null;
+	})
+
+	$('[data-modal="food"]').on('click', () => {
+		$('.food-modal').addClass('active');
+	});
+
+	$('[data-modal]').on('click', () => {
+		$('body').addClass('lock');
+	})
+
+	$('.modal-back').on('click', closeModal);
+	$('.modal-exit').on('click', closeModal);
+
+	function closeModal() {
+		$('.modal').removeClass('active');
+		$('body').removeClass('lock');
+	}
+})
