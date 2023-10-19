6fc6032be4cee1481ffaf2dd6cb739dc2feb9a5c
MonstersBY <monsters.alex0@gmail.com>
2023-10-19 19:38:53 +0300

    feat: detail show

--- src/js/pages/detail_show.js
+++ src/js/pages/detail_show.js
@@ -1,4 +1,26 @@
 import $ from "jquery";
+import Swiper from "swiper";
+import {
+	Navigation,
+	Pagination,
+	Autoplay,
+	EffectFade,
+	EffectCoverflow,
+	Thumbs,
+	EffectCreative,
+	Mousewheel,
+} from "swiper/modules";
+
+function remToPx(remValue) {
+    // Получаем текущий базовый размер шрифта (font-size) из элемента <html>
+    var htmlFontSize = parseFloat(getComputedStyle(document.documentElement).fontSize);
+
+    // Переводим значение из rem в px
+    var pxValue = remValue * htmlFontSize;
+
+    // Округляем значение до целых пикселей (по желанию)
+    return Math.round(pxValue) + 'px';
+}
 
 $('.detail_show-main--open').on("click", function () {
     $(this).css('display', 'none')
@@ -8,3 +30,102 @@ $('.detail_show-questions-item--top').on("click", function () {
     $(this).toggleClass('active')
     $(this).next('.detail_show-questions-item--bottom').slideToggle()
 });
+
+if($('.detail_food').length) {
+    let detail_food
+
+    function detailFoodSwiper() {
+        const cards = document.querySelectorAll('.detail_food-slide')
+        if (screen.width < 769) {
+            cards.forEach(card => {
+                card.classList.add('swiper-slide')
+            })
+            detail_food = new Swiper('.detail_food-swiper', {
+                modules: [Navigation, Pagination, EffectFade],
+                speed: 2000,
+                slidesPerView: 1.065,
+                spaceBetween: `${remToPx(1)}rem`,
+            });
+        } else {
+            cards.forEach(card => {
+                card.classList.remove('swiper-slide')
+              })
+            if(detail_food) {
+                detail_food.destroy()
+                detail_food = undefined
+            }
+        }
+    }
+    
+    detailFoodSwiper();
+    
+    $(window).resize(function () {
+        detailFoodSwiper();
+    });
+}
+
+if($('.detail_decor').length) {
+    let detail_decor
+
+    function detailDecorSwiper() {
+        const cards = document.querySelectorAll('.detail_decor-slide')
+        if (screen.width < 769) {
+            cards.forEach(card => {
+                card.classList.add('swiper-slide')
+            })
+            detail_decor = new Swiper('.detail_decor-swiper', {
+                modules: [Navigation, Pagination, EffectFade],
+                speed: 2000,
+                slidesPerView: 1.065,
+                spaceBetween: `${remToPx(1)}rem`,
+            });
+        } else {
+            cards.forEach(card => {
+                card.classList.remove('swiper-slide')
+              })
+            if(detail_decor) {
+                detail_decor.destroy()
+                detail_decor = undefined
+            }
+        }
+    }
+    
+    detailDecorSwiper();
+    
+    $(window).resize(function () {
+        detailDecorSwiper();
+    });
+}
+
+if($('.detail_photo').length) {
+    let detail_photo
+
+    function detailPhotoSwiper() {
+        const cards = document.querySelectorAll('.detail_photo-slide')
+        if (screen.width < 769) {
+            cards.forEach(card => {
+                card.classList.add('swiper-slide')
+            })
+            detail_photo = new Swiper('.detail_photo-swiper', {
+                modules: [Navigation, Pagination, EffectFade],
+                speed: 2000,
+                slidesPerView: 1.065,
+                spaceBetween: `${remToPx(1)}rem`,
+            });
+        } else {
+            cards.forEach(card => {
+                card.classList.remove('swiper-slide')
+              })
+            if(detail_photo) {
+                detail_photo.destroy()
+                detail_photo = undefined
+            }
+        }
+    }
+    
+    detailPhotoSwiper();
+    
+    $(window).resize(function () {
+        detailPhotoSwiper();
+    });
+}
