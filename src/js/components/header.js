32d937c1731eda1d300032ca382f7f1244d3fca1
MonstersBY <monsters.alex0@gmail.com>
2023-12-06 16:20:09 +0300

    feat: change header logic

--- src/js/components/header.js
+++ src/js/components/header.js
@@ -5,4 +5,15 @@ $(document).ready(function(){
 		$('body').toggleClass('lock');
         $(this).parents('.header-container').toggleClass('active')
 	});
+
+	$(window).scroll(function () {
+		var scrollPosition = $(window).scrollTop()
+
+		if(scrollPosition > 0) {
+			$('.header').addClass('on-top')
+		} else {
+			$('.header').removeClass('on-top')
+		}
+	})
+
 });
